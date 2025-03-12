2ffbd4d660f9b23b0948ac7bc29135ba9995c641
thomas bartier <thomas.bartier@gmail.com>
2025-03-12 11:49:55 +0100

    base app daisy ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/layout/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+     <p className='text-center mt-5'>Bienvenue sur <b className='uppercase'>Töötaja</b></p>
+    </>
+  )
+}
+
+export default App
